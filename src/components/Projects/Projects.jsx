0601a905c4f92fbe1eee23622803b86b1c63c51a
Utsav Negi <unegi@wisc.edu>
2023-04-25 00:35:57 -0500

    Completed Project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,6 +15,11 @@ import b3_bg from "./assets/b3_bg.png"
 import docker_icon from "./assets/icon_docker.png"
 import react_icon from "./assets/icon_react.png"
 import b2_bg from "./assets/b2_bg.png"
+import osm_bg from "./assets/osm_bg.png"
+import pg_icon from "./assets/postgre_logo.png"
+import swift_icon from "./assets/swift_logo.png"
+import ios_icon from "./assets/ios_logo.png"
+import cc_icon from "./assets/cc_logo.png"
 
 export default function Projects(){
     return (
@@ -26,7 +31,7 @@ export default function Projects(){
                     <div id="project" class='card text-light'>
                         <img class="card-img-top" src={ff_icon} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Fuel Finder</h2>
+                            <h3 class="card-title">Fuel Finder</h3>
                             <p class="card-text">An app to show fuel prices of nearby fuel stations and to allow users to create logs to keep track of their fuel expenses.</p>
                             <div id='app_used'>
                                 <img src={icon_android} alt=""/>
@@ -39,8 +44,8 @@ export default function Projects(){
                     <div id="project" class='card text-light'>
                         <img class="card-img-top" src={mask_img} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Face Mask Detection</h2>
-                            <p class="card-text">A machine learning model that uses convolutional neural networks and transfer learning to detect face mask over a given face.</p>
+                            <h3 class="card-title">Face Mask Detection</h3>
+                            <p class="card-text">A ML model that uses convolutional neural networks and transfer learning to detect face mask over a given face.</p>
                             <div id='app_used'>
                                 <img src={tf_icon} alt="" style={{marginLeft:10}}/>
                                 <img src={icon_python} alt="" style={{marginLeft:15}}/>
@@ -50,10 +55,23 @@ export default function Projects(){
                 </div>
                 <div id='project_div' class='col text-light'>
                     <div id="project" class='card text-light'>
+                        <img class="card-img-top" src={cc_icon} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
+                        <div class="card-body">
+                            <h3 class="card-title">Currency Convertor</h3>
+                            <p class="card-text">An app which can convert a given amount of denomination from one currency to another using CurrencyAPI.</p>
+                            <div id='app_used'>
+                                <img src={ios_icon} alt="" style={{marginLeft:-5}}/>
+                                <img src={swift_icon} alt="" style={{marginLeft:15}}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='project_div' class='col text-light'>
+                    <div id="project" class='card text-light'>
                         <img class="card-img-top" src={vae_img} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Variational Autoencoder</h2>
-                            <p class="card-text">A machine learning model that uses neural nets to encode an image to its latent Gaussian state and then decodes it using a distribution sample.</p>
+                            <h3 class="card-title">Variational Autoencoder</h3>
+                            <p class="card-text">A ML model that uses neural nets to encode an image to its latent Gaussian state and then decodes it using a distribution sample.</p>
                             <div id='app_used'>
                                 <img src={pt_icon} alt="" style={{marginLeft:10}}/>
                                 <img src={icon_python} alt="" style={{marginLeft:15}}/>
@@ -63,9 +81,23 @@ export default function Projects(){
                 </div>
                 <div id='project_div' class='col text-light'>
                     <div id="project" class='card text-light'>
+                        <img class="card-img-top" src={osm_bg} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
+                        <div class="card-body">
+                            <h3 class="card-title">Map Tile Generator</h3>
+                            <p class="card-text">A software which can generate identical square-shaped and user-defined OpenStreetMap tiles using Mapnik rendering library. Each element of the software is later containerized in a single pod.</p>
+                            <div id='app_used'>
+                                <img src={docker_icon} alt="" style={{marginLeft:-75}}/>
+                                <img src={icon_python} alt="" style={{marginLeft:15}}/>
+                                <img src={pg_icon} alt="" style={{marginLeft:15}}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='project_div' class='col text-light'>
+                    <div id="project" class='card text-light'>
                         <img class="card-img-top" src={rac_bg} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Prediction of Recycled Aggregate Concrete Properties</h2>
+                            <h3 class="card-title">Prediction of Recycled Aggregate Concrete Properties</h3>
                             <p class="card-text">A set of regression models to predict the properties of recycled aggregate concrete under axisymmetric triaxial load and true triaxial load. These models are then uploaded to a cloud foundry service.</p>
                             <div id='app_used'>
                                 <img src={mastml_icon} alt="" style={{marginLeft:-30}}/>
@@ -78,7 +110,7 @@ export default function Projects(){
                     <div id="project" class='card text-light'>
                         <img class="card-img-top" src={b3_bg} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Bundle Automation Tool v3</h2>
+                            <h3 class="card-title">Bundle Automation Tool v3</h3>
                             <p class="card-text">The third iteration of bundle automation tool which is used to create report summaries of changes that are being migrated to the Human Resource System by uploading audit results from Phire and processing the uploaded data through Jira API.</p>
                             <div id='app_used'>
                                 <img src={st_icon} alt="" style={{marginLeft:-30}}/>
@@ -88,10 +120,10 @@ export default function Projects(){
                     </div>
                 </div>
                 <div id='project_div' class='col text-light'>
-                <div id="project" class='card text-light'>
+                    <div id="project" class='card text-light'>
                         <img class="card-img-top" src={b2_bg} alt="Card cap" style={{height:200, overflow:'hidden'}}/>
                         <div class="card-body">
-                            <h2 class="card-title">Bundle Automation Tool v2</h2>
+                            <h3 class="card-title">Bundle Automation Tool v2</h3>
                             <p class="card-text">The second iteration of bundle automation tool which is used to create report summaries of changes that are being migrated to the Human Resource System by uploading audit results from Phire and processing the uploaded data through Jira API.</p>
                             <div id='app_used'>
                                 <img src={docker_icon} alt="" style={{marginLeft:-75}}/>
@@ -101,21 +133,7 @@ export default function Projects(){
                         </div>
                     </div>
                 </div>
-                <div id='project_div' class='col text-light'>
-                    <div id="project" class='card'>
-                        Project 1
-                    </div>
-                </div>
-                <div id='project_div' class='col text-light'>
-                    <div id="project" class='card'>
-                        Project 1
-                    </div>
-                </div>
-                <div id='project_div' class='col text-light'>
-                    <div id="project" class='card'>
-                        Project 1
-                    </div>
-                </div>
+                
             </div>
         </div>
     )
